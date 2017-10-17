8f785dfe86401483977d23784e53d934690467f6
Arne-Christian Blystad <arne.christian.blystad@bartec-pixavi.com>
2017-10-17 13:38:37 +0200

    Test modifications

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include "mbed.h"
 
 DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+Thread thread;
+
+void led2_thread() {
+    while (true) {
+        led2 = !led2;
+        wait(1);
+    }
+}
 
 // main() runs in its own thread in the OS
 int main() {
+    printf("----------------------------------------\r\n");
+    thread.start(led2_thread);
+
     while (true) {
         led1 = !led1;
         wait(0.5);
+        printf("test\r\n");
     }
 }
 
